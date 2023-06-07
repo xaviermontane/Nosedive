4cf18c9f18a434364ec71757ac793e5f0e39549e
F0RTY <xaviermontane3@gmail.com>
2023-06-07 14:18:25 -0400

    Update Drip.py

--- Nosedive/Drip.py
+++ Nosedive/Drip.py
@@ -1,7 +1,6 @@
-import pygame
-#import sys
+import pygame, sys
 from sprite import Jimmy, Building
-from ButtonClass import Button
+import ButtonClass
 
 # Define screen resolution
 width, height = 700, 500
@@ -31,27 +30,53 @@ font = pygame.font.SysFont("Kanit", 30, True, False)
 character = Jimmy(30, 87)
 all_sprites = pygame.sprite.Group() # Create a list of all sprites
 all_sprites.add(character)
+building_sprites = pygame.sprite.Group()
+building = Building(0, 0) # type: ignore
+building_sprites.add(building)
 running = True # Loop until the user clicks the close button.
 
 # ————————————— Main Program Loop —————————————
-
 while running:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
-    
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-        jump = True
-      #Jimmy.move(character, event.key)
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      mouse_pos = pygame.mouse.get_pos()
+      if event.type == pygame.KEYDOWN:
+        menu = False
+        if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+          character.move("Up")
+
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        mouse_pos = pygame.mouse.get_pos()
+
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_UP:
+          direction = "Stop"
+        elif event.key == pygame.K_DOWN:
+          direction = "Stop"
+        elif event.key == pygame.K_LEFT:
+          direction = "Stop"
+        elif event.key == pygame.K_RIGHT:
+          direction = "Stop"
+      
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+          direction = "Up"
+        elif event.key == pygame.K_DOWN:
+          direction = "Down"
+        elif event.key == pygame.K_LEFT:
+          direction = "Left"
+        elif event.key == pygame.K_RIGHT:
+          direction = "Right"
+        elif event.key == pygame.K_f:
+          print("You payed respects...")
+        
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        print("Click!!")
 
 # ————————————— Game logic should go here
-    
     all_sprites.update()
-
+    building_sprites.update()
 # ————————————— Screen-clearing code
 
     screen.fill(BLACK)
@@ -62,51 +87,24 @@ while running:
     # Draw menu
     menu = True
     while menu is True:
-      title_text = title_font.render("Nosedive", True, BLACK)
+      title_text = title_font.render("Nosedive", True, WHITE)
       start_o = font.render("Start", True, BLACK)
       settings_o = font.render("Settings", True, BLACK)
       quit_o = font.render("Quit", True, BLACK)
-      title_x = 350
-      title_y = 250
-      screen.blit(title_text, (title_x, title_y)) # Blit game title
-    
-      Building(0, 0, 0 ,0)
-
-    all_sprites.draw(screen) # Draw all the sprites
-    screen.blit(character.image, character.rect) # Draw the player (Jimmy)
-
+      screen.blit(title_text, (width, 150)) # Blit game title
+      pygame.draw.rect(screen, WHITE, (300, 300, 100, 50)) # Start button
+      pygame.draw.rect(screen, WHITE, (300, 350, 100, 50)) # Settings button
+      pygame.draw.rect(screen, WHITE, (300, 400, 100, 50)) # Quit button
+      screen.blit(start_o, (320, 310)) # Blit start text
+      screen.blit(settings_o, (310, 360)) # Blit settings text
+      screen.blit(quit_o, (320, 410)) # Blit quit text
     pygame.display.update()
 
-    direction = "Stop"
-
-    # Keyboard inputs
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_UP:
-        direction = "Stop"
-      elif event.key == pygame.K_DOWN:
-        direction = "Stop"
-      elif event.key == pygame.K_LEFT:
-        direction = "Stop"
-      elif event.key == pygame.K_RIGHT:
-        direction = "Stop"
-    
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-        direction = "Jump"
-      elif event.key == pygame.K_DOWN:
-        direction = "Down"
-      elif event.key == pygame.K_LEFT:
-        direction = "Left"
-      elif event.key == pygame.K_RIGHT:
-        direction = "Right"
-      elif event.key == pygame.K_f:
-        print("You payed respects...")
-
-    # Mouse inputs
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      print("Click!!")
-
     # --- Drawing code should go here
+    all_sprites.draw(screen) # Draw all the sprites
+    screen.blit(character.image, character.rect) # Draw the player (Jimmy)
+    building.draw(screen) # Draw the building
+    pygame.display.update()
 
 # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
